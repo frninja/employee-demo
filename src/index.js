ea85d37c4ddb740073b53d515301d745d2335520
Oleg Batashov <krash.se.us@gmail.com>
2018-01-05 19:27:06 +0300

    Добавлен роутер

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { HashRouter } from 'react-router-dom';
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <HashRouter>
+        <App />
+    </HashRouter>
+), document.getElementById('root'));

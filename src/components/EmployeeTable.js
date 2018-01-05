bb9838171ebd9558394cf48d478c315150297424
Oleg Batashov <krash.se.us@gmail.com>
2018-01-05 20:44:03 +0300

    Прописаны маршруты для роутинга

---
+++ src/components/EmployeeTable.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EmployeeTable = () => (
+    <div>
+        <p>Employees table will be around here.</p>
+    </div>
+)
+
+export default EmployeeTable;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import App from './components/App';
+import EmployeeTable from "./components/EmployeeTable";
+import Login from "./components/Login";
 
 ReactDOM.render((
-    <HashRouter>
-        <App />
-    </HashRouter>
+    <Router>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/employees' component={EmployeeTable} />
+        </Switch>
+    </Router>
 ), document.getElementById('root'));
